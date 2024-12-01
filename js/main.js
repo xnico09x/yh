7755676eb336925943eace1cf26718c3072f68ae
xnico09x <160087347+xnico09x@users.noreply.github.com>
2024-12-01 17:14:30 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,345 @@
+
+(function($) {
+    "use strict";
+     $(document).on('ready', function() {
+	
+        jQuery(window).on('scroll', function() {
+			if ($(this).scrollTop() > 200) {
+				$('#header .header-inner').addClass("sticky");
+			} else {
+				$('#header .header-inner').removeClass("sticky");
+			}
+		});
+
+		function setSizes() {
+			var margin_top = 100;
+			var margin_right = 85;
+			var margin_bottom = 10;
+			var margin_left = 85;
+		
+			// get image width and height
+			var img_w = $('.gallery_img').width();
+			var img_h = $('.gallery_img').height();
+			// calculate viewport width and height
+			var vp_w = $(window).width() - margin_right - margin_left;
+			var vp_h = $(window).height() - margin_top - margin_bottom;
+			//
+			if (vp_w <= img_w || vp_w > img_w) {
+				// new width
+				var img_w_new=vp_w;
+				// calculate new height
+				var img_h_new=Math.round((img_h*img_w_new) / img_w);
+			}
+
+			document.querySelectorAll('.menu > li > a').forEach(item => {
+				item.addEventListener('click', event => {
+				  const dropdown = event.target.nextElementSibling;
+				  if (dropdown && dropdown.classList.contains('dropdown')) {
+					dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+				  }
+				});
+			  });
+			//
+			if (vp_h <= img_h || vp_h > img_h) {
+				// new height
+				var img_h_new=vp_h;
+				// calculate new width
+				var img_w_new=Math.round((img_w*img_h_new) / img_h);
+			}
+			// change image width and height to new width and new height
+			$('.gallery_img').width(img_w_new);
+			$('.gallery_img').height(img_h_new);
+		}
+		
+		// onload
+		$(window).load(function(){ setSizes(); });
+		// on resize
+		$(window).bind("resize", function() { setSizes(); });
+		
+		/*====================================
+			Sticky Header JS
+		======================================*/ 
+		jQuery(window).on('scroll', function() {
+			if ($(this).scrollTop() > 100) {
+				$('.header').addClass("sticky");
+			} else {
+				$('.header').removeClass("sticky");
+			}
+		});
+		
+		$('.pro-features .get-pro').on( "click", function(){
+			$('.pro-features').toggleClass('active');
+		});
+		
+		/*====================================
+			Search JS
+		======================================*/ 
+		$('.search a').on( "click", function(){
+			$('.search-top').toggleClass('active');
+		});
+		
+		/*====================================
+			Mobile Menu
+		======================================*/ 	
+		$('.menu').slicknav({
+			prependTo:".mobile-nav",
+			duration: 300,
+			closeOnClick:true,
+			
+
+ });
+		
+		/*===============================
+			Hero Slider JS
+		=================================*/ 
+		$(".hero-slider").owlCarousel({
+			loop:true,
+			autoplay:true,
+			smartSpeed: 500,
+			autoplayTimeout:3500,
+			singleItem: true,
+			autoplayHoverPause:true,
+			items:1,
+			nav:true,
+			navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+			dots:false,
+		});
+
+		/*===============================
+			Testimonial Slider JS
+		=================================*/ 
+		$('.testimonial-slider').owlCarousel({
+			items:3,
+			autoplay:true,
+			autoplayTimeout:4500,
+			smartSpeed:300,
+			autoplayHoverPause:true,
+			loop:true,
+			merge:true,
+			nav:false,
+			dots:true,
+			responsive:{
+				1: {
+					items:1,
+				},
+				300: {
+					items:1,
+				},
+				480: {
+					items:1,
+				},
+				768: {
+					items:2,
+				},
+				1170: {
+					items:3,
+				},
+			}
+		});
+		
+		/*===============================
+			Portfolio Slider JS
+		=================================*/ 
+		$('.portfolio-slider').owlCarousel({
+			autoplay:true,
+			autoplayTimeout:4000,
+			margin:15,
+			smartSpeed:300,
+			autoplayHoverPause:true,
+			loop:true,
+			nav:true,
+			dots:false,
+			responsive:{
+				300: {
+					items:1,
+				},
+				480: {
+					items:2,
+				},
+				768: {
+					items:2,
+				},
+				1170: {
+					items:4,
+				},
+			}
+		});
+		
+		/*=====================================
+			Counter Up JS
+		======================================*/
+		$('.counter').counterUp({
+			delay:20,
+			time:2000
+		});
+		
+		/*===============================
+			Clients Slider JS
+		=================================*/ 
+		$('.clients-slider').owlCarousel({
+			items:5,
+			autoplay:true,
+			autoplayTimeout:3500,
+			margin:15,
+			smartSpeed: 400,
+			autoplayHoverPause:true,
+			loop:true,
+			nav:false,
+			dots:false,
+			responsive:{
+				300: {
+					items:1,
+				},
+				480: {
+					items:2,
+				},
+				768: {
+					items:3,
+				},
+				1170: {
+					items:5,
+				},
+			}
+		});
+		
+		/*====================================
+			Single Portfolio Slider JS
+		======================================*/ 
+		$('.pf-details-slider').owlCarousel({
+			items:1,
+			autoplay:false,
+			autoplayTimeout:5000,
+			smartSpeed: 400,
+			autoplayHoverPause:true,
+			loop:true,
+			merge:true,
+			nav:true,
+			dots:false,
+			navText: ['<i class="icofont-rounded-left"></i>', '<i class="icofont-rounded-right"></i>'],
+		});
+		
+		/*===================
+			Accordion JS
+		=====================*/ 
+		$('.accordion > li:eq(0) a').addClass('active').next().slideDown();
+		$('.accordion a').on('click', function(j) {
+			var dropDown = $(this).closest('li').find('p');
+			$(this).closest('.accordion').find('p').not(dropDown).slideUp(300);
+			if ($(this).hasClass('active')) {
+				$(this).removeClass('active');
+			} else {
+				$(this).closest('.accordion').find('a.active').removeClass('active');
+				$(this).addClass('active');
+			}
+			dropDown.stop(false, true).slideToggle(300);
+			j.preventDefault();
+		});
+		
+		/*====================================
+			Nice Select JS
+		======================================*/ 	
+		$('select').niceSelect();
+		
+		/*=====================================
+			Date Picker JS
+		======================================*/ 
+		$( function() {
+			$( "#datepicker" ).datepicker();
+		} );
+		
+		
+		
+		/*===============================
+			Checkbox JS
+		=================================*/  
+		$('input[type="checkbox"]').change(function(){
+			if($(this).is(':checked')){
+				$(this).parent("label").addClass("checked");
+			} else {
+				$(this).parent("label").removeClass("checked");
+			}
+		});
+		
+		/*===============================
+			Right Bar JS
+		=================================*/ 
+		$('.right-bar .bar').on( "click", function(){
+			$('.sidebar-menu').addClass('active');
+		});
+		$('.sidebar-menu .cross').on( "click", function(){
+			$('.sidebar-menu').removeClass('active');
+		});
+		
+		/*=====================
+			Video Popup JS
+		=======================*/ 
+		$('.video-popup').magnificPopup({
+			type: 'video',	
+		});
+		
+		/*================
+			Wow JS
+		==================*/		
+		var window_width = $(window).width();   
+			if(window_width > 767){
+            new WOW().init();
+		}
+	
+		/*===================
+			Scroll Up JS
+		=====================*/
+		$.scrollUp({
+			scrollText: '<span><i class="fa fa-angle-up"></i></span>',
+			easingType: 'easeInOutExpo',
+			scrollSpeed: 900,
+			animation: 'fade'
+		}); 
+
+		/*=======================
+			Animate Scroll JS
+		=========================*/
+		$('.scroll').on("click", function (e) {
+			var anchor = $(this);
+				$('html, body').stop().animate({
+					scrollTop: $(anchor.attr('href')).offset().top - 100
+				}, 1000);
+			e.preventDefault();
+		});
+		
+		/*=======================
+			Stellar JS
+		=========================*/
+		$.stellar({
+		  horizontalOffset: 0,
+		  verticalOffset: 0
+		});
+
+		/*====================
+			Google Maps JS
+		======================*/
+		var map = new GMaps({
+				el: '#map',
+				lat: 23.011245,
+				lng: 90.884780,
+				scrollwheel: false,
+			});
+			map.addMarker({
+				lat: 23.011245,
+				lng: 90.884780,
+				title: 'Marker with InfoWindow',
+				infoWindow: {
+				content: '<p>welcome to Medipro</p>'
+			}
+		
+		});
+	});
+	
+	/*====================
+		Preloader JS
+	======================*/
+	$(window).on('load', function() {
+		$('.preloader').addClass('preloader-deactivate');
+	});
+	
+	
+})(jQuery);
